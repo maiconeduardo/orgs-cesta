51b52796ed0b32981444295f13646a0144af9d2e
maiconeduardo <maicon.eduardo@outlook.com>
2023-07-24 15:21:57 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StatusBar, SafeAreaView } from 'react-native';
+import Cesta from './src/telas/Cesta';
+
+
+export default function App() {
+  return (
+
+     <SafeAreaView>
+        <StatusBar />
+        <Cesta />
+     </SafeAreaView>
+     
+
+   
+  
+  );
+}
+
---
+++ src/telas/Cesta.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Image, StyleSheet } from "react-native";
+import topo from '../../assets/topo.png';
+
+export default function Cesta()
+{
+    return (
+
+       <Image source={topo} style={estilos.topo} />
+
+    )    
+
+    const estilos = StyleSheet.create();
+    topo: { 
+        width: "100%",
+
+    }
+    
+}
+
