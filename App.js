ca60857ae8450c2087d18933e7ef079ce1d9f1d6
maiconeduardo <maicon.eduardo@outlook.com>
2023-07-24 20:36:03 -0300

    Segundo

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-import { StatusBar, SafeAreaView } from 'react-native';
+import { StatusBar, SafeAreaView, View } from 'react-native';
 import Cesta from './src/telas/Cesta';
-
+import { setStatusBarHidden } from "expo-status-bar";
+import { useFonts,NotoSans_700Bold, NotoSans_400Regular  } from '@expo-google-fonts/noto-sans';
 
 export default function App() {
+   let [fonteCarregada] = useFonts({
+      "NotoSansBold": NotoSans_700Bold, 
+      "NotoSansRegular":NotoSans_400Regular,
+     });
+   
+     if (!fonteCarregada) {
+       return <View />;
+     }
   return (
 
      <SafeAreaView>
--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,13 +1,41 @@
 import React from "react";
-import { Image, StyleSheet, Dimensions, Text } from "react-native";
+import { Image, StyleSheet, Dimensions, Text, View, ScrollView } from "react-native";
+import Texto from '../componentes/Texto.js';
 import topo from '../../assets/topo.png';
+import logo from '../../assets/logo.png';
+
+
 const width = Dimensions.get('screen').width;
+
+
+
 export default function Cesta()
 {
-    return <>
-       <Image source={topo} style={estilos.topo} />
-       <Text style={estilos.titulo}> Detalhes da cesta</Text>
 
+    return <>
+        {/* Em source é usado topo para enviar o caminho da imagens que se encontra em import, diferente do style que adiciona o estilo*/}
+        
+            <Image source={topo} style={estilos.topo} />
+           
+            <Texto style={estilos.titulo}> Detalhes da cesta</Texto>
+            <View style={estilos.container} >
+                    <Texto style={estilos.nome}>Cesta de verduras</Texto>
+                    <View style={estilos.imgLogo}>
+
+                            <Image source={logo} style={estilos.logo}  />
+                            <Texto style={estilos.nomeFazenda}>Jenny Jack Farm</Texto>
+
+                    </View>
+                    
+                    <Texto style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para cozinhar
+                    </Texto>
+                    
+                    
+
+                    <Texto style={estilos.preco}>R$: 40,00 </Texto>
+
+            </View>
+       
        </>
 
        
@@ -15,15 +43,71 @@ export default function Cesta()
 
 const estilos = StyleSheet.create({
 
-topo: {
-    width: "100%",
-    height: 578/768 * width,
-},
-titulo: {
-    width: "100%",
-    position: "absolute",
-    textAlign: "Center",
+    topo: {
+        width: "100%",
+        height: 578/768 * width,
+    },
+ 
+    titulo: {
+        width: "100%",
+        position: "absolute",
+        textAlign: "center",
+        fontSize: 16,
+        lineHeight: 20,
+        color: "white",
+        padding: 16,
+        fontWeight: "bold",
+      
 
-}
+    },
 
-})
+    container:{
+        paddingVertical: 8,
+        paddingHorizontal:16,
+    },
+
+    nome: {
+      fontSize: 26,
+      lineHeight: 42,
+      color: "#464646",
+      fontWeight: "bold",
+      
+
+    },
+
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 26,
+        fontWeight: "bold",
+        
+    },
+
+    descricao: {
+    color: "#A3A3A3",
+    fontSize: 16,
+    lineHeight: 26,
+    
+    },
+
+    preco: {
+        color: "#2A9F85",
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8,
+        fontWeight: "bold",
+       
+    },
+    logo: {
+        width: 32,
+        height: 32,
+
+    },
+    imgLogo: {
+        gap:10,
+        flexDirection: "row",
+        
+        
+
+    }
+
+});
